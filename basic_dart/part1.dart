4b83f3cfeaa595cd123f53d98c40a75ec6ecdde8
Halil İbrahim Buğday <halilbugday03@gmail.com>
2024-01-27 10:43:06 +0300

    basic dart processes

---
+++ basic_dart/part1.dart
@@ -0,0 +1,15 @@
+main(List<String> args) {
+/* Print process */
+  print("object1");
+  print('object2');
+  print("3 + 5"); //str
+  print(3 + 5); //int
+  print('Halil\'s Lectures'); //void // or ("Halil's Lectures")
+  print("Halil" + "Bugday"); //merge
+
+/* Variable & Data types */
+
+
+
+
+}
