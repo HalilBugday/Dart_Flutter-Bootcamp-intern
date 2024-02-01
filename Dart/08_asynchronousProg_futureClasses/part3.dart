2a816595d9ad69f14f6c28a822ed2b4f47f02b8e
Halil İbrahim Buğday <halilbugday03@gmail.com>
2024-02-01 19:36:11 +0300

    Error fixed

--- Dart/08_asynchronousProg_futureClasses/part3.dart
+++ Dart/08_asynchronousProg_futureClasses/part3.dart
@@ -1,5 +1,19 @@
 void main(List<String> args) {
   //https://dart.dev/guides --> everything about dart!!!
   /* Async and Await */
-  
+  //yukarı async keyword yazdık, program çalışmaya devam eder
+  print("Personal data will be fetched from the internet.");
+  processAboutPerson();
+  print("Program running still..");
+}
+
+Future<String> getDataFronNet() {
+  return Future<String>.delayed(Duration(seconds: 3), () {
+    return "Name: 'Halil', ID: '28'";
+  });
+}
+
+void processAboutPerson() async {
+  String person = await getDataFronNet();
+  print("Length: ${person.length}");
 }
---
+++ Dart/09_nullsafety/part1.dart
@@ -0,0 +1,8 @@
+void main(List<String> args) {
+  //https://dart.dev/guides --> everything about dart!!!
+  /*Nullable, non-nullable, assertion*/
+
+  int number1;
+  number1 = null;
+  print("The value of number1: $number1");
+}
