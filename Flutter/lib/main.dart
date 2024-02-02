5f2442eb9b173cfd5183605abaa862f1bce1977e
Halil İbrahim Buğday <halilbugday03@gmail.com>
2024-02-02 21:20:43 +0300

    Flutter builded

---
+++ Flutter/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Pamukkale University'),
+          backgroundColor: Colors.blue,
+        ),
+        body: Text('Hello world!'),
+      ),
+    );
+  }
+}
